3bf50e97ea743455a0915dc61e70c79bedf1ce88
blogggowthamen <36755466+blogggowthamen@users.noreply.github.com>
2018-02-23 14:46:42 +0530

    Update Vowel or Consonant.py

--- beginner/Vowel or Consonant.py
+++ beginner/Vowel or Consonant.py
@@ -2,5 +2,7 @@ num= raw_input("Enter the Character:")
 list = ['a','e','i','o','u','A','E','I','O','U']
 if num in list:
     print("Vowel")
+elif num>=0:
+    print("Invaild")
 else:
     print("Consonant")
