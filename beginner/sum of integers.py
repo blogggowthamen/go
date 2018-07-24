c649951f03e3390080bb6666705e83e3a300e595
blogggowthamen <36755466+blogggowthamen@users.noreply.github.com>
2018-07-24 09:55:40 +0530

    Update sum of integers.py

--- beginner/sum of integers.py
+++ beginner/sum of integers.py
@@ -1,10 +1,10 @@
-n=int(input("enter the number1"))
-k=int(input("enter the number2"))
+N=int(input("enter the number1"))
+K=int(input("enter the number2"))
 d=[]
 sum=0
-for i in range(0,n):
+for i in range(0,N):
   e=int(input("enter the list of element "))
   d.append(e)
-for j in range(0,k):
+for j in range(0,K):
   sum=sum+d[j]
 print(sum)
